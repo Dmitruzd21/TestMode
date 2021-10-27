6c081712bdffe096ba3be3ba2b5862eff96778e4
Korotkov Dmitry <2110821@mail.ru>
2021-10-27 19:01:54 +0300

    initial commit

---
+++ src/test/java/ru/netology/LoginThroughTheWebInterfaceTest.java
@@ -0,0 +1,7 @@
+package ru.netology;
+
+public class LoginThroughTheWebInterfaceTest {
+
+    //для запуска SUT: java -jar app-ibank.jar -P:profile=test
+    //java -jar C:\Users\79168\Desktop\SoftwareTestingEngineer\AutomationHomework\TestMode\artifacts\app-ibank.jar -P:profile=test
+}

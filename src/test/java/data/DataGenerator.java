d8d987a5ccb8c7ea190b462bed7436c0a391a410
Korotkov Dmitry <2110821@mail.ru>
2021-10-28 22:54:54 +0300

    DataGenerator is prepared

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -1,4 +1,75 @@
 package data;
 
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+import lombok.Value;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
 public class DataGenerator {
+    public DataGenerator() {
+    }
+
+    // Создаем дата-класс пользователя, а также при помощи Loombok автоматически создаем конструктор
+    // на основе этого класса будут созданы пользователи с разным статусом (через конструктор напрямую инициилизируем поля)
+
+    @Value
+    public static class UserInfo {
+        String login;
+        String password;
+        String status;
+    }
+
+    // Создадим объект класса faker c заданной локалью
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    // Универсальные методы генерации данных (логина и пароля),а для этого перед этим создадим объект класса faker c заданной локалью
+    public static String getLogin (){
+        return faker.name().username();
+    }
+
+    public static String getPassword (){
+        return faker.internet().password();
+    }
+
+    // Создаем активного пользователя
+    public static UserInfo createActiveUser(){
+        UserInfo user = new UserInfo(getLogin(),getPassword(),"active");
+        sendRequestToCreateUser (user); // отправь логин,пароль и статус в тело запроса
+        return user; // эти же данные используй в тесте
+    }
+
+    // Создаем заблокированного пользователя
+    public static UserInfo createBlockedUser (){
+        UserInfo user = new UserInfo(getLogin(),getPassword(),"blocked");
+        sendRequestToCreateUser (user);  // отправь логин,пароль и статус в тело запроса
+        return user; // эти же данные используй в тесте
+    }
+
+    // Данные пользователей теперь необходимо передать в тело запроса. Для этого создадим класс, который отправляет запросы к SUT
+
+    // Спецификация, которая используется при отправке запроса (указывается в .spec)
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static void sendRequestToCreateUser(UserInfo user) {
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
 }
+
--- src/test/java/ru/netology/LoginThroughTheWebInterfaceTest.java
+++ src/test/java/ru/netology/LoginThroughTheWebInterfaceTest.java
@@ -3,12 +3,14 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
+import data.DataGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Condition.*;
+import static data.DataGenerator.*;
 
 public class LoginThroughTheWebInterfaceTest {
 
@@ -26,17 +28,19 @@ public class LoginThroughTheWebInterfaceTest {
     // 1 - Наличие активного пользователя (успешный вход)
     @Test
     public void shouldLoginRegisteredUser() {
-        $("[data-test-id=login] .input__control").setValue("vasya");
-        $("[data-test-id=password] .input__control").setValue("password");
+        DataGenerator.UserInfo activeUser = createActiveUser();
+        $("[data-test-id=login] .input__control").setValue(activeUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(activeUser.getPassword());
         $("[data-test-id=action-login]").click();
         $(Selectors.withText("Личный кабинет")).shouldBe(visible);
     }
 
     // 2 - Наличие заблокированного пользователя
     @Test
-    public void shouldNotLoginIfUserIsBlocked() {
-        $("[data-test-id=login] .input__control").setValue("Ivan");
-        $("[data-test-id=password] .input__control").setValue("passwordG");
+    public void shouldGetErrorIfUserIsBlocked() {
+        DataGenerator.UserInfo blockedUser = createBlockedUser();
+        $("[data-test-id=login] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(blockedUser.getPassword());
         $("[data-test-id=action-login]").click();
         $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
         $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Пользователь заблокирован"));
@@ -44,9 +48,9 @@ public class LoginThroughTheWebInterfaceTest {
 
     // 3 - Отсутствие пользователя
     @Test
-    public void shouldNotLoginIfUserDoesNotExist() {
-        $("[data-test-id=login] .input__control").setValue("ggg");
-        $("[data-test-id=password] .input__control").setValue("fw44w3");
+    public void shouldGetErrorIfUserDoesNotExist() {
+        $("[data-test-id=login] .input__control").setValue(getLogin());
+        $("[data-test-id=password] .input__control").setValue(getPassword());
         $("[data-test-id=action-login]").click();
         $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
         $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
@@ -54,9 +58,10 @@ public class LoginThroughTheWebInterfaceTest {
 
     // 4 - Невалидный логин активного пользователя
     @Test
-    public void shouldNotLoginIfActiveUserWithInvalidLogin() {
-        $("[data-test-id=login] .input__control").setValue("vasy");
-        $("[data-test-id=password] .input__control").setValue("password");
+    public void shouldGetErrorIfActiveUserWithInvalidLogin() {
+        DataGenerator.UserInfo activeUser = createActiveUser();
+        $("[data-test-id=login] .input__control").setValue(getLogin());
+        $("[data-test-id=password] .input__control").setValue(activeUser.getPassword());
         $("[data-test-id=action-login]").click();
         $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
         $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
@@ -64,9 +69,10 @@ public class LoginThroughTheWebInterfaceTest {
 
     // 5 - Невалидный пароль активного пользователя
     @Test
-    public void shouldNotLoginIfActiveUserWithInvalidPassword() {
-        $("[data-test-id=login] .input__control").setValue("vasya");
-        $("[data-test-id=password] .input__control").setValue("greg");
+    public void shouldGetErrorIfActiveUserWithInvalidPassword() {
+        DataGenerator.UserInfo activeUser = createActiveUser();
+        $("[data-test-id=login] .input__control").setValue(activeUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(getPassword());
         $("[data-test-id=action-login]").click();
         $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
         $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
@@ -74,9 +80,10 @@ public class LoginThroughTheWebInterfaceTest {
 
     // 6 - Невалидный логин заблокированного пользователя
     @Test
-    public void shouldNotLoginIfBlockedUserWithInvalidLogin() {
-        $("[data-test-id=login] .input__control").setValue("Iva");
-        $("[data-test-id=password] .input__control").setValue("passwordG");
+    public void shouldGetErrorIfBlockedUserWithInvalidLogin() {
+        DataGenerator.UserInfo blockedUser = createBlockedUser();
+        $("[data-test-id=login] .input__control").setValue(getLogin());
+        $("[data-test-id=password] .input__control").setValue(blockedUser.getPassword());
         $("[data-test-id=action-login]").click();
         $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
         $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
@@ -84,9 +91,10 @@ public class LoginThroughTheWebInterfaceTest {
 
     // 7 - Невалидный пароль заблокированного пользователя
     @Test
-    public void shouldNotLoginIfBlockedUserWithInvalidPassword() {
-        $("[data-test-id=login] .input__control").setValue("Ivan");
-        $("[data-test-id=password] .input__control").setValue("htrh");
+    public void shouldGetErrorIfBlockedUserWithInvalidPassword() {
+        DataGenerator.UserInfo blockedUser = createBlockedUser();
+        $("[data-test-id=login] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(getPassword());
         $("[data-test-id=action-login]").click();
         $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
         $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
